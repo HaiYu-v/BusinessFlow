9f591490fcd0413729b3c6a7e21a10c67585f2aa
HaiYu <2239473866@qq.com>
2024-12-12 20:01:45 +0800

    业务流测试

---
+++ src/test/java/org/example/designs/business_flow/BusinessTest.java
@@ -0,0 +1,77 @@
+package org.example.designs.business_flow;
+
+import org.example.designs.business_flow.cache.GlobalCache;
+import org.example.designs.business_flow.cache.TemporaryCache;
+import org.example.designs.business_flow.core.BusinessFlow;
+import org.example.designs.business_flow.core.BusinessFlowException;
+import org.example.designs.business_flow.core.IChain;
+import org.example.designs.conver.Test1;
+import org.junit.jupiter.api.Test;
+
+/**
+ * TODO
+ *
+ * <p>
+ *     TODO
+ * </p>
+ *
+ * @author HaiYu
+ * @version 1.0.0
+ * @date 2024-12-12
+ */
+public class BusinessTest {
+    @Test
+    void test1(){
+        try {
+            BusinessFlow businessFlow = BusinessFlow.build("测试业务流性能");
+            for(int i=0 ;i<100; i++){
+                businessFlow.add(Chain1.class,"performance");
+            }
+
+            businessFlow.getGlobalValueCache().put("test2",new Test1(1,"xiaoming",1.0));
+            businessFlow
+                    .add(Chain1.class, "start")
+                    .add(Chain1.class, "conver")
+                    .add(Chain1.class, "converTest2")
+                    .start();
+//            System.out.println(end.getName());
+
+//            StringBuilder stringBuilder = new StringBuilder();
+//            System.out.println(businessFlow.getInfoJSON());
+            System.out.println(businessFlow.getVisualLog());
+
+        } catch (BusinessFlowException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void test2(){
+        TestController controller = new TestController();
+        System.out.println(controller.test());
+    }
+
+    @Test
+    void test3() {
+
+
+        BusinessFlow businessFlow = null;
+        try {
+            businessFlow = BusinessFlow
+                    .build("基本类型转换测试")
+                    .add("匿名类测试", "ret", new IChain() {
+                        @Override
+                        public Object method(TemporaryCache temporaryCache, GlobalCache globalCache) {
+                            return "匿名类执行";
+                        }
+                    });
+//                    .add(Chain1.class, "start1")
+//                    .add(Chain1.class, "conver1")
+
+        } catch (BusinessFlowException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println(businessFlow.getVisualLog());
+        }
+    }
+}
